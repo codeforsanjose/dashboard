7ecd85af7ccbc89967bc0ec2b428f6dd0f542485
Erso <erikavasnormandy@gmail.com>
2020-01-29 21:41:06 -0800

    code for san jose materials

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown.js';
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import github from './github.png';
 import axios from 'axios';
 import CardComponent from './CardComponent.js';
@@ -68,35 +68,31 @@ axios.get(END_POINT + API_KEY).
         
         <div className="App-header" style={divStyle}>
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Countdown to Ames</h2>
-
-
-         <a className = "designer">
+ 
+	<ul className="designer">
+	<li >
+         <a  href ="https://api.nasa.gov/index.html" target="_blank">
           <img className="smallPin" src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"/>
-          <a href="https://api.nasa.gov/index.html"><span>NASA Picture of The Day: {this.state.contents.title}, courtesy of the NASA Public API</span></a>
+          <a href="https://api.nasa.gov/index.html"><span>NASA Picture of The Day: {this.state.contents.title}, Public API</span></a>
 
-         </a>
-
-          <a className="designer" href="https://github.com/kristinbaumann/react-countdown" target="_blank">
-            <img src={github} alt="github" className="githubIcon" />
+         </a></li>
+<li>
+          <a href="https://github.com/kristinbaumann/react-countdown" target="_blank">
+            <img src={github} className="smallPin" alt="github" />
             <span>Forked from Kristin Baumann</span>
-          </a>
+          </a></li>
+
+	</ul>
         </div>
 
 
           <div className="AtomicClock">
-            <h3 className="title">September 2020</h3>
-            <Countdown date={`2020-09-18T00:00:00`} />
+		<h2>Welcome to Code For San Jose, a Branch of Code For America</h2>
+		<h3>Start Time: 6:45pm </h3>
+		<h3>End Time: 8:45pm </h3>
+          	<h2>		<Countdown date={`2020-01-23T20:45:00`} /></h2>
 
-          <p className="Payload"><h3>Payload:</h3> To bring the latest and greatest tech and security that the Bay Area has to offer to NASA</p>
-          <p>Stay long enough to build up these skillsets. By the time the countdown reaches 0, come back again.</p>
-            <p>I suppose I should be more formal about it but at this point I just want something to tell myself that I have an endgoal in mind.</p>
-        
-       
-            <MainComponent/>
-            <SideQuestsComponent/>
 
-            <CertsComponent/>
 
          </div>
 
